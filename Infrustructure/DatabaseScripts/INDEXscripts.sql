bf1fc70f2575b94331b8aabc90664caf80fc5699
Jamshid <jtursunov092@gmail.com>
2023-08-18 14:04:28 +0500

    index has been added

---
+++ Infrustructure/DatabaseScripts/INDEXscripts.sql
@@ -0,0 +1,8 @@
+﻿--Index for quarterId column in person table since it had to be filtered by this column most in docs in both client and user
+-- which inherts from 
+
+CREATE INDEX IF NOT EXISTS by_quarter
+    ON public.person USING btree
+    (quarter_id );
+
+
